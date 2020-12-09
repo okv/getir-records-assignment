8b2bf442deca46d194b3669f9937cf1f634cb82d
Oleg Korobenko <oleg.korobenko@gmail.com>
2020-12-10 02:36:11 +0300

    add implementation, integration tests, doc

--- README.md
+++ README.md
@@ -1,3 +1,99 @@
 # getir records assignment
 
-WIP...
+
+## Requirements
+
+* Node.js >= 14
+* MongoDb > 4
+
+
+## Service configuration
+
+Service could be configured using environment variables:
+
+* **PORT**: number -- port to listen, the default value is 3030
+* **HOST**: string -- host to listen, the default value is "127.0.0.1"
+* **MONGODB_URI**: string -- mongodb connection string (should contain db name),
+the default value is "mongodb://127.0.0.1/getir-case-study"
+* **LOG_LEVEL**: string -- log level, the default value is "info"
+* **JSON_BODY_LIMIT**: string -- body parser limit for json, the default value
+is "10kb"
+
+
+## Service API
+
+### POST /records
+
+Returns records according to the request body parameters.
+
+Body parameters (json), **all parameters are required**:
+
+* **startDate** - date in a "YYYY-MM-DD" format, picks records with
+`createdAt >= startDate`
+* **endDate** - date in a "YYYY-MM-DD" format, picks records with
+`createdAt < endDate`
+* **minCount** - picks records with `totalCount >= minCount`
+* **maxCount** - picks records with `totalCount <= maxCount`
+
+Sample:
+
+```json
+{
+	"startDate": "2015-01-01",
+	"endDate": "2015-01-03",
+	"minCount": 1,
+	"maxCount": 164
+}
+```
+
+Response payload (json):
+
+* **code** - status code of the request, 0 - success, in the case of the errored
+request will contain error code.
+* **msg** - message for the status code, "Success" for successful requests, will
+contain explanatory message otherwise.
+* **records** - filtered items according to the request
+
+Sample:
+
+```json
+{
+	"code": 0,
+	"msg": "Success",
+	"records": [
+	{
+		"key": "yPfeOYkP",
+		"createdAt": "2015-01-02T21:02:42.536Z",
+		"totalCount": 164
+	},
+	{
+		"key": "bQkcWXyU",
+		"createdAt": "2015-01-02T14:24:03.408Z",
+		"totalCount": 85
+	}
+	]
+}
+```
+
+
+## Development
+
+Make sure that [requirements](#requirements) are satisfied.
+
+Prepare dev environment:
+
+* `npm ci` to install project dependencies
+* `TEST_MONGODB_URI="mongodb://127.0.0.1/getir-case-study-testdb" npm test` to run tests
+
+Start service in development mode with any [app options](#service-configuration)
+e.g.:
+
+```
+MONGODB_URI="mongodb://127.0.0.1/getir-case-study" npm run dev
+```
+
+Now you can use service e.g. via curl:
+
+```
+curl -H 'content-type: application/json' -X POST http://127.0.0.1:3030/records -d '{"startDate": "2015-01-01", "endDate": "2015-01-03", "minCount": 1, "maxCount": 164}'
+```
---
+++ test/integration/records/getItems.js
@@ -0,0 +1,201 @@
+const assert = require('assert');
+const fetch = require('node-fetch');
+const expressUtils = require('../../../lib/utils/express');
+const createApp = require('../../../lib/app');
+
+describe('records getItems', () => {
+	let app;
+	let server;
+	let oldMongodbUri;
+	let recordsUrl;
+
+	before(async () => {
+		oldMongodbUri = process.env.MONGODB_URI;
+		process.env.MONGODB_URI = (
+			process.env.TEST_MONGODB_URI ||
+			'mongodb://127.0.0.1/getir-case-study-testdb'
+		);
+		app = await createApp({loggerOptions: {level: 'fatal'}});
+		server = app.listen();
+		await expressUtils.waitForServer(server);
+		const {port} = server.address();
+		recordsUrl = `http://127.0.0.1:${port}/records`;
+	});
+
+	after(async () => {
+		if (oldMongodbUri) process.env.MONGODB_URI = oldMongodbUri;
+		if (server) await expressUtils.closeServer(server);
+		const mongoClient = app?.get('mongoClient');
+		if (mongoClient) await mongoClient.close();
+	});
+
+	describe('without request payload', () => {
+		it('should return expected response', async () => {
+			const response = await fetch(recordsUrl, {
+				method: 'POST'
+			});
+			const result = await response.json();
+			assert.strictEqual(
+				response.status,
+				400,
+				'response should have status 400'
+			);
+			assert.deepStrictEqual(result, {
+				code: 400,
+				msg: 'Request is malformed'
+			}, 'response content should be as expected');
+		});
+	});
+
+	describe('with startDate == endDate', () => {
+		it('should return expected response', async () => {
+			const response = await fetch(recordsUrl, {
+				method: 'POST',
+				body: JSON.stringify({
+					startDate: '2020-01-01',
+					endDate: '2020-01-01',
+					minCount: 30,
+					maxCount: 70
+				}),
+				headers: {'content-type': 'application/json'}
+			});
+			const result = await response.json();
+			assert.strictEqual(
+				response.status,
+				400,
+				'response should have status 400'
+			);
+			assert.deepStrictEqual(result, {
+				code: 400,
+				msg: 'Request is malformed'
+			}, 'response content should be as expected');
+		});
+	});
+
+	describe('with minCount > maxCount', () => {
+		it('should return expected response', async () => {
+			const response = await fetch(recordsUrl, {
+				method: 'POST',
+				body: JSON.stringify({
+					startDate: '2020-01-01',
+					endDate: '2020-01-02',
+					minCount: 71,
+					maxCount: 70
+				}),
+				headers: {'content-type': 'application/json'}
+			});
+			const result = await response.json();
+			assert.strictEqual(
+				response.status,
+				400,
+				'response should have status 400'
+			);
+			assert.deepStrictEqual(result, {
+				code: 400,
+				msg: 'Request is malformed'
+			}, 'response content should be as expected');
+		});
+	});
+
+	describe('with valid date and count filters but no match', () => {
+		before(async () => {
+			const records = [{
+				createdAt: new Date('2020-01-01T00:00:00'),
+				counts: [10, 20],
+				key: 'key1'
+			}];
+			const recordsCol = app.get('db').collection('records');
+			await recordsCol.insertMany(records);
+		});
+
+		after(async () => {
+			const recordsCol = app.get('db').collection('records');
+			await recordsCol.deleteMany({});
+		});
+
+		it('should return expected response', async () => {
+			const response = await fetch(recordsUrl, {
+				method: 'POST',
+				body: JSON.stringify({
+					startDate: '2020-01-01',
+					endDate: '2020-01-02',
+					minCount: 70,
+					maxCount: 70
+				}),
+				headers: {'content-type': 'application/json'}
+			});
+			const result = await response.json();
+			assert.strictEqual(
+				response.status,
+				200,
+				'response should have status 200'
+			);
+			assert.deepStrictEqual(result, {
+				code: 0,
+				msg: 'Success',
+				records: []
+			}, 'response content should be as expected');
+		});
+	});
+
+	describe('with valid date and count filters and matched records', () => {
+		before(async () => {
+			const records = [{
+				createdAt: new Date('2020-01-02T00:00:00.000Z'),
+				counts: [10, 20],
+				key: 'key4'
+			}, {
+				createdAt: new Date('2020-01-01T18:00:00.000Z'),
+				counts: [50, 60],
+				key: 'key3'
+			}, {
+				createdAt: new Date('2020-01-01T12:00:00.000Z'),
+				counts: [30, 40],
+				key: 'key2'
+			}, {
+				createdAt: new Date('2020-01-01T00:00:00.000Z'),
+				counts: [10, 20],
+				key: 'key1'
+			}];
+			const recordsCol = app.get('db').collection('records');
+			await recordsCol.insertMany(records);
+		});
+
+		after(async () => {
+			const recordsCol = app.get('db').collection('records');
+			await recordsCol.deleteMany({});
+		});
+
+		it('should return expected response', async () => {
+			const response = await fetch(recordsUrl, {
+				method: 'POST',
+				body: JSON.stringify({
+					startDate: '2020-01-01',
+					endDate: '2020-01-02',
+					minCount: 30,
+					maxCount: 70
+				}),
+				headers: {'content-type': 'application/json'}
+			});
+			const result = await response.json();
+			assert.strictEqual(
+				response.status,
+				200,
+				'response should have status 200'
+			);
+			assert.deepStrictEqual(result, {
+				code: 0,
+				msg: 'Success',
+				records: [{
+					createdAt: new Date('2020-01-01T12:00:00.000Z').toISOString(),
+					totalCount: 70,
+					key: 'key2'
+				}, {
+					createdAt: new Date('2020-01-01T00:00:00.000Z').toISOString(),
+					totalCount: 30,
+					key: 'key1'
+				}]
+			}, 'response content should be as expected');
+		});
+	});
+});
